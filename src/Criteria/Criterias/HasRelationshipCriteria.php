4c7c2c817654767fb213f4845b7ff88b3b1a3651
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-11 11:40:29 -0300

    atualiza HasRelationshipCriteria

---
+++ src/Criteria/Criterias/HasRelationshipCriteria.php
@@ -0,0 +1,29 @@
+<?php
+namespace LincolnBrito\LaravelBaseRepositories\Criteria\Criterias;
+
+use LincolnBrito\LaravelBaseRepositories\Criteria\Criteria;
+use LincolnBrito\LaravelBaseRepositories\Contracts\RepositoryInterface as Repository;
+
+class HasRelationshipCriteria extends Criteria {
+
+    /** @var  string */
+    protected $relationship;
+
+    /**
+     * HasRelationshipCriteria constructor.
+     * @param $relationship
+     */
+    public function __construct($relationship){
+        $this->relationship = $relationship;
+    }
+
+    /**
+     * @param $model
+     * @param Repository $repository
+     * @return mixed
+     */
+    public function apply($model, Repository $repository) {
+        $query = $model->has($this->relationship);
+        return $query;
+    }
+}
