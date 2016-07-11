28833d1ce12b1d34c90d691fe481d8ecab16554c
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-11 11:31:20 -0300

    criado GreaterThanCriteria

---
+++ src/Criteria/Criterias/GreaterThanCriteria.php
@@ -0,0 +1,34 @@
+<?php
+namespace LincolnBrito\LaravelBaseRepositories\Criteria\Criterias;
+
+use LincolnBrito\LaravelBaseRepositories\Criteria\Criteria;
+use LincolnBrito\LaravelBaseRepositories\Contracts\RepositoryInterface as Repository;
+
+class GreaterThanCriteria extends Criteria {
+
+    /** @var  string */
+    protected $attribute;
+
+    /** @var  mixed */
+    protected $value;
+
+    /**
+     * LessThan constructor.
+     * @param $atribute
+     * @param $value
+     */
+    public function __construct($atribute, $value){
+        $this->attribute = $atribute;
+        $this->value = $value;
+    }
+
+    /**
+     * @param $model
+     * @param Repository $repository
+     * @return mixed
+     */
+    public function apply($model, Repository $repository) {
+        $query = $model->where($this->attribute, '>', $this->value);
+        return $query;
+    }
+}
