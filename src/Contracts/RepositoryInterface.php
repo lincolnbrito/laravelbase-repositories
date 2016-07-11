65d8cba00c9915e879d973ca503742a47bba99a4
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-11 09:59:03 -0300

    atualiza phpdoc

--- src/Contracts/RepositoryInterface.php
+++ src/Contracts/RepositoryInterface.php
@@ -3,17 +3,51 @@ namespace LincolnBrito\LaravelBaseRepositories\Contracts;
 
 interface RepositoryInterface {
 
+    /**
+     * @param array $columns
+     * @return mixed
+     */
     public function all($columns = array('*'));
 
+    /**
+     * @param int $perPage
+     * @param array $columns
+     * @return mixed
+     */
     public function paginate($perPage = 15, $columns = array('*'));
 
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public function create(array $data);
 
+    /**
+     * @param array $data
+     * @param $id
+     * @param string $attribute
+     * @return mixed
+     */
     public function update(array $data, $id, $attribute = "id");
 
+    /**
+     * @param array $ids
+     * @return mixed
+     */
     public function delete(array $ids);
 
+    /**
+     * @param $id
+     * @param array $columns
+     * @return mixed
+     */
     public function find($id, $columns = array('*'));
 
+    /**
+     * @param $attribute
+     * @param $value
+     * @param array $columns
+     * @return mixed
+     */
     public function findBy($attribute, $value, $columns = array('*'));
 }
