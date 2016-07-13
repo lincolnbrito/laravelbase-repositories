527947e579d187c6b14363d389206bf9977f023f
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-13 11:56:09 -0300

    Permitindo atribuição de valores dinamicamente nos critérios

--- src/Criteria/Criteria.php
+++ src/Criteria/Criteria.php
@@ -1,11 +1,43 @@
 <?php
 namespace LincolnBrito\LaravelBaseRepositories\Criteria;
 
-use Illuminate\Http\Request;
 use LincolnBrito\LaravelBaseRepositories\Contracts\RepositoryInterface as Repository;
 
 abstract class Criteria
 {
+    protected $values = [];
+    protected $fill;
+
+    /**
+     * Criteria constructor.
+     * @param $values
+     */
+    public function __construct($values){
+        foreach ($values as $key => $val){
+            if(in_array($key, $this->fill))
+                $this->values[$key] = $val;
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
+    {
+        if(in_array($name, $this->fill))
+            $this->values[$name] = $value;
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if(in_array($name, $this->fill))
+            return $this->values[$name];
+    }
 
     /**
      * @param $model
